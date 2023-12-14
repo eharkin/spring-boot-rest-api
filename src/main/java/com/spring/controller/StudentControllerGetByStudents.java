3977134e0d83d6f40b3c2fbc7d1f079428a3b287
Harkin, Emmett <emmett.harkin@optum.com>
2023-12-14 20:35:53 +0000

    update controller

--- src/main/java/com/spring/controller/StudentControllerGetByStudents.java
+++ src/main/java/com/spring/controller/StudentControllerGetByStudents.java
@@ -4,6 +4,7 @@ import com.spring.dto.StudentDTO;
 import com.spring.service.StudentServiceGetStudentsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/students")
 public class StudentControllerGetByStudents {
 
     // Dependency Injection of Service Layer
     @Autowired
     StudentServiceGetStudentsImpl studentServiceGetStudents;
 
-    @GetMapping
+    @GetMapping(value = "getAllStudents", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<StudentDTO> getStudents() {
         return studentServiceGetStudents.getStudents();
